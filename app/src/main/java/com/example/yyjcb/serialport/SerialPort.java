05475b5b62a4803367d9eafaac4f2821ead96ee8
陆伟 <yyjcb@nfcp-yyjcbdeMacBook-Air.local>
2017-10-18 16:53:54 +0800

    项目配置

---
+++ app/src/main/java/com/example/yyjcb/serialport/SerialPort.java
@@ -0,0 +1,16 @@
+package com.example.yyjcb.serialport;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 陆伟 2017/10/18
+ */
+public class SerialPort extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serial_port);
+    }
+}
